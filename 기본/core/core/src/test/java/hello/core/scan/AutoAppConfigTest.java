b75f16043482c8f93540d391bd5795585597bbd0
mine702 <mine702@naver.com>
2023-09-24 23:12:12 +0800

    spring

--- 기본/core/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ 기본/core/core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -14,7 +14,7 @@ public class AutoAppConfigTest {
     void basicScan() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
-        MemberService memberService = ac.getBean("memberService2", MemberService.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         assertThat(memberService).isInstanceOf(MemberService.class);
 
