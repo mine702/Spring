96a3cb973cb3657ed55501d4825dc3fb7a047ca4
mine702 <mine702@naver.com>
2023-10-22 00:04:44 +0900

    spring

--- 기본/core/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ 기본/core/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,11 +1,15 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class MemoryMemberRepository implements MemberRepository{
+@Component
+public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
+
     @Override
     public void save(Member member) {
         store.put(member.getId(), member);
