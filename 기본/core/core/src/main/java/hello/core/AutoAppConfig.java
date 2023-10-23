923867d3fdf6fe7f51b77d1c1587ac26dc179ad2
mine702 <mine702@naver.com>
2023-10-23 22:33:05 +0900

    중복등록과 충돌

--- 기본/core/core/src/main/java/hello/core/AutoAppConfig.java
+++ 기본/core/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 수동 빈 등록 이 자동 빈 등록 보다 우선권을 가지는데 springboot 에서는 오류 메시지를 발생한다
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
