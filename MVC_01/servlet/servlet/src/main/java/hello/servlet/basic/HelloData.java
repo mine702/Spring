c3ccd6f6e2f6ef50973b92aef91ed4b3cdacf43d
mine702 <mine702@naver.com>
2023-12-13 21:36:56 +0900

    api메시지 바디 - json

---
+++ MVC_01/servlet/servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,14 @@
+package hello.servlet.basic;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class HelloData {
+
+    private String username;
+    private int age;
+}
