c8ae3d2074ad50c92c984df31131473be757397c
mine702 <mine702@naver.com>
2023-12-23 00:12:31 +0900

    블록

--- MVC_02/thymeleaf-basic/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ MVC_02/thymeleaf-basic/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -126,4 +126,10 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/comments";
     }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
 }
