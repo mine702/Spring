9ecde83b97519229e8570613452a30cf848af0f8
mine702 <mine702@naver.com>
2023-12-23 00:31:50 +0900

    fragment

---
+++ MVC_02/thymeleaf-basic/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/TemplateController.java
@@ -0,0 +1,14 @@
+package hello.thymeleafbasic.basic;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/template")
+public class TemplateController {
+    @GetMapping("/fragment")
+    public String template() {
+        return "template/fragment/fragmentMain";
+    }
+}
