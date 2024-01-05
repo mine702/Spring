acecf21878e622ff7f5582dfa3ff7e54f7f675dc
mine702 <mine702@naver.com>
2024-01-05 09:47:01 +0900

    수정 삭제

--- DB_01/jdbc/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ DB_01/jdbc/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -24,6 +26,15 @@ class MemberRepositoryV0Test {
         log.info("findMember={}", findMember);
         assertThat(findMember).isEqualTo(member);
 
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        //delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 
 
