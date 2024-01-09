9f62154eae8de4253a384f1015a2c9da87e3d38e
mine702 <mine702@naver.com>
2024-01-09 23:38:18 +0900

    적용

--- DB_02/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ DB_02/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -1,6 +1,6 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV2Config;
+import hello.itemservice.config.MyBatisConfig;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -11,7 +11,11 @@ import org.springframework.context.annotation.Profile;
 
 
 @Slf4j
-@Import(JdbcTemplateV2Config.class)
+//@Import(MemoryConfig.class)
+//@Import(JdbcTemplateV1Config.class)
+//@Import(JdbcTemplateV2Config.class)
+//@Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
