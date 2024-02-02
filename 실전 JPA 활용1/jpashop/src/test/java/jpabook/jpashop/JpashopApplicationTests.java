c7652a05520914a813b913ab0fa3ff6de1fcacb0
mine702 <mine702@naver.com>
2024-02-02 23:51:41 +0900

    init

---
+++ 실전 JPA 활용1/jpashop/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class JpashopApplicationTests {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    @Transactional
+    public void testMember() {
+        Member member = new Member();
+        member.setUsername("memberA");
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
+    }
+}
---
+++ 실전 JPA 활용1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+
+class MemberRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void find() {
+    }
+}
