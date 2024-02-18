06e350619a0abf528d318e3b3d7148a32791353c
mine702 <mine702@naver.com>
2024-02-18 23:16:27 +0900

    v6

--- 실전 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ 실전 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -18,8 +18,16 @@ public class OrderQueryDto {
     private Address address;
     private List<OrderItemQueryDto> orderItems;
 
-    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate,
-                         OrderStatus orderStatus, Address address) {
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
         this.orderId = orderId;
         this.name = name;
         this.orderDate = orderDate;
--- 실전 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ 실전 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -74,7 +74,7 @@ public class OrderQueryRepository {
         return result.stream()
                 .map(OrderQueryDto::getOrderId)
                 .collect(Collectors.toList());
-        
+
     }
 
     private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
@@ -87,5 +87,16 @@ public class OrderQueryRepository {
                 .getResultList();
         return orderItems.stream()
                 .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d" +
+                                " join o.orderItems oi" +
+                                " join oi.item i", OrderFlatDto.class)
+                .getResultList();
     }
 }
