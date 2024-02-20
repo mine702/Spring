ea9fdbd37d0bc61a0834f09a94cf707780abab8e
mine702 <mine702@naver.com>
2024-02-20 11:00:47 +0900

    도메인 모델

--- 데이터 JPA/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ 데이터 JPA/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import study.datajpa.entitiy.Member;
+import study.datajpa.entity.Member;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
