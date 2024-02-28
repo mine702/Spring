e2c47978dba5237b7db78b8b9a35fbbcb416b46b
김민건 <mine702@naver.com>
2024-02-28 17:40:08 +0900

    jpa 완

--- QueryDSL/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ QueryDSL/querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -30,5 +32,33 @@ class MemberRepositoryTest {
         assertThat(result1).containsExactly(member);
         List<Member> result2 = memberRepository.findByUsername("member1");
         assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void querydslPredicateExecutor() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        Iterable<Member> result = memberRepository.findAll(
+                member.age
+                        .between(10, 40)
+                        .and(member.username.eq("member1"))
+        );
+
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
     }
 }
