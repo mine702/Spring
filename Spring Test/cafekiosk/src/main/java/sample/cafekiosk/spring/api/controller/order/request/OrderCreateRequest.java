462eb847523661c327151799895f7282645caac7
mine702 <mine702@naver.com>
2024-03-10 23:43:38 +0900

    http test

--- Spring Test/cafekiosk/src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
+++ Spring Test/cafekiosk/src/main/java/sample/cafekiosk/spring/api/controller/order/request/OrderCreateRequest.java
@@ -2,13 +2,15 @@ package sample.cafekiosk.spring.api.controller.order.request;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class OrderCreateRequest {
 
-    private final List<String> productNumbers;
+    private List<String> productNumbers;
 
     @Builder
     private OrderCreateRequest(List<String> productNumbers) {
