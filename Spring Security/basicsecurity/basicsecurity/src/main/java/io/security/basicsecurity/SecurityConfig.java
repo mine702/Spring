692f6391fb4d9f5af443b8ee5d980cad47b058aa
mine702 <mine702@naver.com>
2024-03-24 23:03:10 +0900

    시큐리티 버전 다운

--- Spring Security/basicsecurity/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ Spring Security/basicsecurity/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,57 +1,21 @@
 package io.security.basicsecurity;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http)throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
-                        authorizeRequests
-                                .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults())
-                .formLogin(formLogin ->
-                        formLogin
-                                .loginPage("/loginPage")
-                                .defaultSuccessUrl("/")
-                                .usernameParameter("userId")
-                                .passwordParameter("passwd")
-                                .loginProcessingUrl("/login_proc")
-                                .successHandler(new AuthenticationSuccessHandler() {
-                                    @Override
-                                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                                        System.out.println("authentication : "+ authentication.getName());
-                                        response.sendRedirect("/");
-                                    }
-                                })
-                                .failureHandler(new AuthenticationFailureHandler() {
-                                    @Override
-                                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                                        System.out.println("exception : "+ exception.getMessage());
-                                        response.sendRedirect("/login");
-                                    }
-                                })
-                                .permitAll()
-                );
-        return http.build();
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+
     }
 }
