d53e93ca253773c84050f63c36a9208f49a9af71
mine702 <mine702@naver.com>
2024-07-20 22:45:27 +0900

    구체 클래스 기반 프록시 - 예제2

--- 고급/proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
+++ 고급/proxy/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteClient.java
@@ -1,7 +1,7 @@
 package hello.proxy.pureproxy.concreteproxy.code;
 
 public class ConcreteClient {
-    private final ConcreteLogic concreteLogic;
+    private final ConcreteLogic concreteLogic;  //ConcreteLogic, TimeProxy 모두 주입 가능
 
     public ConcreteClient(ConcreteLogic concreteLogic) {
         this.concreteLogic = concreteLogic;
