66be6516b5bfe7b5468c13addd823d7a2b963fc2
mine <mine702@naver.com>
2024-10-25 11:22:55 +0900

    데코레이터 패턴 - 예제코드1

---
+++ 고급/proxy/src/test/java/hello/proxy/decorator/code/RealComponent.java
@@ -0,0 +1,12 @@
+package hello.proxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class RealComponent implements Component {
+    @Override
+    public String operation() {
+        log.info("RealComponent operation 호출");
+        return "DATA";
+    }
+}
