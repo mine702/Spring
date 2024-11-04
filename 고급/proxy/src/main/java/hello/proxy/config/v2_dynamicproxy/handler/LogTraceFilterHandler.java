6c29fb110d634e95ade533f66d7c26236d5bc96a
mine <mine702@naver.com>
2024-11-04 17:20:38 +0900

    JDK 동적 프록시 적용 2

--- 고급/proxy/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
+++ 고급/proxy/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
@@ -13,25 +13,22 @@ public class LogTraceFilterHandler implements InvocationHandler {
     private final LogTrace logTrace;
     private final String[] patterns;
 
-    public LogTraceFilterHandler(Object target, LogTrace logTrace, String...
-            patterns) {
+    public LogTraceFilterHandler(Object target, LogTrace logTrace, String... patterns) {
         this.target = target;
         this.logTrace = logTrace;
         this.patterns = patterns;
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws
-            Throwable {
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         //메서드 이름 필터
         String methodName = method.getName();
-        if (!PatternMatchUtils.simpleMatch(patterns, methodName)) {
+        if (!PatternMatchUtils.simpleMatch(patterns, methodName))
             return method.invoke(target, args);
-        }
+
         TraceStatus status = null;
         try {
-            String message = method.getDeclaringClass().getSimpleName() + "."
-                    + method.getName() + "()";
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
             status = logTrace.begin(message);
             //로직 호출
             Object result = method.invoke(target, args);
