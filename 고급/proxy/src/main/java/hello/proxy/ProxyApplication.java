4208ef3425c5af293982dd39b1773917946e637a
mine <mine702@naver.com>
2024-11-08 16:27:11 +0900

    프록시 팩토리 - 적용2

--- 고급/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 고급/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,7 +2,7 @@ package hello.proxy;
 
 //import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ConcreteProxyConfig.class)
-@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
     public static void main(String[] args) {
