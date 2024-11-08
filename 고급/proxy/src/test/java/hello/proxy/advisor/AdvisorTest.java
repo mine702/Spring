0cf8890e6d09875ad79c7713ced0b012df7e147c
mine <mine702@naver.com>
2024-11-08 15:49:03 +0900

    예제 코드3 - 스프링이 제공하는 포인트컷

--- 고급/proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ 고급/proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -11,6 +11,7 @@ import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import java.lang.reflect.Method;
 
@@ -34,6 +35,21 @@ public class AdvisorTest {
         ServiceInterface service = new ServiceImpl();
         ProxyFactory proxyFactory = new ProxyFactory(service);
         DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    @Test
+    @DisplayName("스프링이 제공하는 포인트 컷")
+    void advisorTest3() {
+        ServiceInterface service = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(service);
+        NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
+        pointcut.setMappedName("save");
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(pointcut, new TimeAdvice());
         proxyFactory.addAdvisor(advisor);
         ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
 
