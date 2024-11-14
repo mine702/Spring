b9592c1ffe715e9c0ec1a7815ce4e413863cb7d5
mine <mine702@naver.com>
2024-11-14 13:44:47 +0900

    Aspect 프록시 - 적용

--- 고급/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 고급/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,7 +2,7 @@ package hello.proxy;
 
 //import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 
-import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
+import hello.proxy.config.v6_aop.AopConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -17,7 +17,8 @@ import org.springframework.context.annotation.Import;
 //@Import(ConcreteProxyConfig.class)
 //@Import(ProxyFactoryConfigV2.class)
 //@Import(BeanPostProcessorConfig.class)
-@Import(AutoProxyConfig.class)
+//@Import(AutoProxyConfig.class)
+@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
     public static void main(String[] args) {
