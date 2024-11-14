d2e3f049cd46653570ec4f646ef62fc7a0ef8ba8
mine <mine702@naver.com>
2024-11-14 11:14:55 +0900

    스프링이 제공하는 빈 후처리기1

--- 고급/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 고급/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,7 +2,7 @@ package hello.proxy;
 
 //import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 
-import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -16,7 +16,8 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(ProxyFactoryConfigV2.class)
-@Import(BeanPostProcessorConfig.class)
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
     public static void main(String[] args) {
